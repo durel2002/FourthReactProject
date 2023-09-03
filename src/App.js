9ad74334e9cb4d6da1bfa3a3237688b112f81e91
durel2002 <durelhervane@gmail.com>
2023-09-03 01:26:15 +0100

    This is my finial list of item App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
 function App() {
+  const arry=["meat", "fish", "egg", "rice","bread",'spagehti',3];
   return (
     <>
+    <ol type="i">
+    <h3>
+{arry.map((item,i) => <li key={i}> <b>{item}</b> </li>)}
+    </h3>
+    </ol>
     </>
   );
 }
